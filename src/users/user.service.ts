bd87cf14f0f8d0d0d887d503b40ebc40f63a4111
Muhammad Usman <usman4276@gmail.com>
2023-02-16 20:32:57 +0500

    crud continue

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,14 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/users.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UserServices {
-  GetUser(): string {
-    return 'user has got';
+  public user: UserDto[] = [];
+
+  AddUser(user: UserDto): string {
+    //id auto-generated
+    user.id = uuidv4();
+    this.user.push(user);
+    return 'user added successfully';
   }
-  DeleteUser(): string {
-    return 'user is deleted';
+  GetUser(): UserDto[] {
+    return this.user;
+  }
+  DeleteUser(id: string): string {
+    //filtering the user by id from the array of objects
+    const delUserIndex = this.user.filter((currentObj) => {
+      if (currentObj.id === id) return this.user.indexOf(currentObj);
+    });
+    console.log(
+      '🚀 ~ file: user.service.ts:23 ~ UserServices ~ delUserIndex ~ delUserIndex',
+      delUserIndex,
+    );
+
+    // if(delUserIndex) this.user.splice(delUserIndex,1);
+    return 'user deleted successfully';
   }
   UpdateUser(): string {
-    return 'user is updated';
+    return 'user is updatedd';
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,35 @@
-import { Controller, Delete, Get, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UserDto } from './dto/users.dto';
 import { UserServices } from './user.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private userServices: UserServices) {}
 
+  //Add user
+  @Post('/addUser')
+  addUser(@Body() user: UserDto): string {
+    return this.userServices.AddUser(user);
+  }
+
   //Get Users
   @Get('/getUsers')
-  findAll(): string {
+  findAll(): UserDto[] {
     return this.userServices.GetUser();
   }
 
   //Delete Users
-  @Delete('/deleteUsers')
-  deleteUsers(): string {
-    return this.userServices.DeleteUser();
+  @Delete('/deleteUsers/:id')
+  deleteUsers(@Param('id') id: string): string {
+    return this.userServices.DeleteUser(id);
   }
 
   //Update Users
